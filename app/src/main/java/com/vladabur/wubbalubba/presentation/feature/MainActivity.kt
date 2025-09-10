4440909c3c14caa0b2ed1f9a9656a20d5ace76eb
VladyslavaBuriakovska <buriakovska.vladyslava@gmail.com>
2025-09-11 00:00:37 +0300

    feat(navigation): add nav host to the activity

--- app/src/main/java/com/vladabur/wubbalubba/presentation/feature/MainActivity.kt
+++ app/src/main/java/com/vladabur/wubbalubba/presentation/feature/MainActivity.kt
@@ -1,9 +1,10 @@
-package com.vladabur.wubbalubba.ui.feature
+package com.vladabur.wubbalubba.presentation.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,22 +12,29 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.vladabur.wubbalubba.ui.theme.WubbaLubbaTheme
+import androidx.navigation.compose.rememberNavController
+import com.vladabur.wubbalubba.presentation.navigation.MainNavHost
+import com.vladabur.wubbalubba.presentation.ui.theme.WubbaLubbaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
+
             WubbaLubbaTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    content = { innerPadding ->
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            MainNavHost(navController)
+                        }
+                    }
+                )
             }
         }
     }
