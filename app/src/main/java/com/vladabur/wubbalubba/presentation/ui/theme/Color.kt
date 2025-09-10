f3e6c5809b03cbd1dbeb3dbd588de5a18d05a156
VladyslavaBuriakovska <buriakovska.vladyslava@gmail.com>
2025-09-11 00:13:16 +0300

    feat(base): change colors, typo and theme

--- app/src/main/java/com/vladabur/wubbalubba/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/vladabur/wubbalubba/presentation/ui/theme/Color.kt
@@ -1,11 +1,8 @@
 package com.vladabur.wubbalubba.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightPrimaryRed = Color(0xFFD9493F)
+val LightBackground = Color(0xFFF5F5F5)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnSurface = Color(0xFF212121)
+val LightOnSurfaceVariant = Color(0xFF757575)
