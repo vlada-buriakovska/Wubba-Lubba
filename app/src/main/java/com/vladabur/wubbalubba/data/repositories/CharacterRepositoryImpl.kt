169d04b7c9ada7c022a2656d1a9ac02915f0dcec
VladyslavaBuriakovska <buriakovska.vladyslava@gmail.com>
2025-09-11 11:26:11 +0300

    feat(search): update get characters use case to include search by name

--- app/src/main/java/com/vladabur/wubbalubba/data/repositories/CharacterRepositoryImpl.kt
+++ app/src/main/java/com/vladabur/wubbalubba/data/repositories/CharacterRepositoryImpl.kt
@@ -1,9 +1,8 @@
 package com.vladabur.wubbalubba.data.repositories
 
-import com.vladabur.wubbalubba.data.network.models.CharacterResponse
+import com.vladabur.wubbalubba.data.extensions.mapToApiErrors
 import com.vladabur.wubbalubba.data.network.models.CharactersListResponse
 import com.vladabur.wubbalubba.data.network.services.CharacterService
-import com.vladabur.wubbalubba.domain.models.Character
 import com.vladabur.wubbalubba.domain.models.CharactersList
 import com.vladabur.wubbalubba.domain.repositories.CharacterRepository
 import javax.inject.Inject
@@ -12,8 +11,12 @@ import javax.inject.Inject
 class CharacterRepositoryImpl @Inject constructor(private val characterService: CharacterService) :
     CharacterRepository {
 
-    override suspend fun getCharactersList(page: Int): CharactersList {
-        val response = characterService.getCharacters(page)
-        return CharactersListResponse.map(response)
+    override suspend fun getCharactersList(page: Int, name: String?): CharactersList {
+        return try {
+            val response = characterService.getCharacters(page, name)
+            CharactersListResponse.map(response)
+        } catch (e: Exception) {
+            throw e.mapToApiErrors()
+        }
     }
 }
