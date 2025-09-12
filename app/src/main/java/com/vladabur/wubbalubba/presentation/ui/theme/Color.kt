39ed89dbc169e3b4b9d2fc90eff87e7125b9ba55
VladyslavaBuriakovska <buriakovska.vladyslava@gmail.com>
2025-09-12 12:23:25 +0300

    feat(dark theme): add colors for dark theme and dark color scheme

--- app/src/main/java/com/vladabur/wubbalubba/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/vladabur/wubbalubba/presentation/ui/theme/Color.kt
@@ -8,3 +8,12 @@ val LightBackground = Color(0xFFF5F5F5)
 val LightSurface = Color(0xFFFFFFFF)
 val LightOnSurface = Color(0xFF212121)
 val LightOnSurfaceVariant = Color(0xFF757575)
+
+
+val DarkPrimaryRed = Color(0xFFFF6B6B)
+val DarkOnPrimary = Color(0xFF1C1C1C)
+val DarkSecondary = Color(0xFFAAAAAA)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnSurface = Color(0xFFECECEC)
+val DarkOnSurfaceVariant = Color(0xFFB0B0B0)
